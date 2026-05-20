1cec746aad591cd1fd4681c271dc54b4001d97d3
Donmac4671 <oseim697@gmail.com>
2026-05-20 18:02:00 +0000

    Create AnnouncementsPage component with AdminLayout

---
+++ src/pages/AnnouncementsPage.tsx
@@ -0,0 +1,14 @@
+import AdminLayout from "../layouts/AdminLayout";
+
+export default function AnnouncementsPage() {
+  return (
+    <AdminLayout>
+      <h1 style={{
+        fontSize: "42px",
+        fontWeight: "900",
+      }}>
+        Announcements
+      </h1>
+    </AdminLayout>
+  );
+}
