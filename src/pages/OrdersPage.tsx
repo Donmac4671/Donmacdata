f50e26ec545f3c4c9a52c733d4828260464b0adb
Donmac4671 <oseim697@gmail.com>
2026-05-20 16:07:48 +0000

    Create OrdersPage component with AdminLayout

---
+++ src/pages/OrdersPage.tsx
@@ -0,0 +1,11 @@
+import AdminLayout from "../layouts/AdminLayout";
+
+export default function OrdersPage() {
+  return (
+    <AdminLayout>
+      <h1 className="text-4xl font-bold text-white">
+        Orders
+      </h1>
+    </AdminLayout>
+  );
+}
