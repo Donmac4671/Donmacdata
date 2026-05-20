1e705db0ee7fa7e6ad8884180cca1f00243b351a
Donmac4671 <oseim697@gmail.com>
2026-05-20 15:55:18 +0000

    Refactor routes and import statements in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
+
 import AdminDashboard from "./pages/AdminDashboard";
+
 import ResellersPage from "./pages/ResellersPage";
+
+import CustomersPage from "./pages/CustomersPage";
+
+import OrdersPage from "./pages/OrdersPage";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
+      <Route
+        path="/"
+        element={<LoginPage />}
+      />
 
       <Route
         path="/admin"
@@ -25,6 +37,24 @@ export default function App() {
         element={
           <ProtectedRoute>
             <ResellersPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/admin/customers"
+        element={
+          <ProtectedRoute>
+            <CustomersPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/admin/orders"
+        element={
+          <ProtectedRoute>
+            <OrdersPage />
           </ProtectedRoute>
         }
       />
