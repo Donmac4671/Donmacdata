6b02ec13353ad6bbeaf7e3535f356ab024946616
Donmac4671 <oseim697@gmail.com>
2026-05-20 17:49:33 +0000

    Create TopupsPage component with AdminLayout

---
+++ src/pages/TopupsPage.tsx
@@ -0,0 +1,16 @@
+import AdminLayout from "../layouts/AdminLayout";
+
+export default function TopupsPage() {
+  return (
+    <AdminLayout>
+      <h1
+        style={{
+          fontSize: "42px",
+          fontWeight: "900",
+        }}
+      >
+        Top Ups
+      </h1>
+    </AdminLayout>
+  );
+}
