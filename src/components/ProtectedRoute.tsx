40f1793976e640ebba76c514a3a6082a723875a7
Donmac4671 <oseim697@gmail.com>
2026-05-20 12:57:00 +0000

    Simplify ProtectedRoute by removing isLoggedIn check
    Remove login check from ProtectedRoute component.

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,15 +1,7 @@
-import { Navigate } from "react-router-dom";
-
 export default function ProtectedRoute({
   children,
 }: {
   children: JSX.Element;
 }) {
-  const isLoggedIn = true;
-
-  if (!isLoggedIn) {
-    return <Navigate to="/" />;
-  }
-
   return children;
 }
