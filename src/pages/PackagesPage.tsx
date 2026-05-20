7414fa198efdb46ff43ffd96946f62f03209de64
Donmac4671 <oseim697@gmail.com>
2026-05-20 17:52:01 +0000

    Create PackagesPage component with AdminLayout

---
+++ src/pages/PackagesPage.tsx
@@ -0,0 +1,14 @@
+import AdminLayout from "../layouts/AdminLayout";
+
+export default function PackagesPage() {
+  return (
+    <AdminLayout>
+      <h1 style={{
+        fontSize: "42px",
+        fontWeight: "900",
+      }}>
+        Packages
+      </h1>
+    </AdminLayout>
+  );
+}
