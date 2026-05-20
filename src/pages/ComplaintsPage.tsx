011420ccb9a70c64f20d159804be160ee5a8fade
Donmac4671 <oseim697@gmail.com>
2026-05-20 17:53:51 +0000

    Create ComplaintsPage component with AdminLayout

---
+++ src/pages/ComplaintsPage.tsx
@@ -0,0 +1,14 @@
+import AdminLayout from "../layouts/AdminLayout";
+
+export default function ComplaintsPage() {
+  return (
+    <AdminLayout>
+      <h1 style={{
+        fontSize: "42px",
+        fontWeight: "900",
+      }}>
+        Complaints
+      </h1>
+    </AdminLayout>
+  );
+}
