8792f00d9843cc5976045ae6bf536cefea24cd73
Donmac4671 <oseim697@gmail.com>
2026-05-20 17:51:18 +0000

    Create RankingsPage component with AdminLayout

---
+++ src/pages/RankingsPage.tsx
@@ -0,0 +1,14 @@
+import AdminLayout from "../layouts/AdminLayout";
+
+export default function RankingsPage() {
+  return (
+    <AdminLayout>
+      <h1 style={{
+        fontSize: "42px",
+        fontWeight: "900",
+      }}>
+        Top Reseller Rankings
+      </h1>
+    </AdminLayout>
+  );
+}
