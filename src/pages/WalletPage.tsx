2fc4424fcfd016096e50cd09a45584e242535b2b
Donmac4671 <oseim697@gmail.com>
2026-05-21 08:10:58 +0000

    Create WalletPage.tsx

---
+++ src/pages/WalletPage.tsx
@@ -0,0 +1,26 @@
+export default function WalletPage() {
+  return (
+    <div
+      style={{
+        minHeight:
+          "100vh",
+        background:
+          "#020617",
+        color: "white",
+        padding:
+          "40px",
+      }}
+    >
+      <h1
+        style={{
+          fontSize:
+            "48px",
+          fontWeight:
+            "900",
+        }}
+      >
+        Wallet
+      </h1>
+    </div>
+  );
+}
