ffc14210882b9967bf079a4788d00289661320e3
Donmac4671 <oseim697@gmail.com>
2026-05-21 01:12:07 +0000

    Update PackagesPage.tsx

--- src/pages/PackagesPage.tsx
+++ src/pages/PackagesPage.tsx
@@ -12,8 +12,8 @@ export default function PackagesPage() {
     "AirtelTigo Premium",
     "AirtelTigo Big Time",
     "Mashup",
-    "Voice + SMS",
-    "Voice + Data + SMS",
+    "Telecel Voice + SMS",
+    "Telecel Voice + Data + SMS",
     "Airtime",
     "Special Offers",
   ];
@@ -488,6 +488,424 @@ export default function PackagesPage() {
       active: true,
     },
 
+// =========================
+// MASHUP
+// =========================
+
+{
+  id: 54,
+  category: "Mashup",
+  name:
+    "¢1 - 15.27MB & 15.64 Minutes",
+  price: "GH₵1",
+  active: true,
+},
+
+{
+  id: 55,
+  category: "Mashup",
+  name:
+    "¢2 - 30.53MB & 31.27 Minutes",
+  price: "GH₵2",
+  active: true,
+},
+
+{
+  id: 56,
+  category: "Mashup",
+  name:
+    "¢3 - 48.8MB & 46.92 Minutes",
+  price: "GH₵3",
+  active: true,
+},
+
+{
+  id: 57,
+  category: "Mashup",
+  name:
+    "¢4 - 61.07MB & 62.55 Minutes",
+  price: "GH₵4",
+  active: true,
+},
+
+{
+  id: 58,
+  category: "Mashup",
+  name:
+    "¢5 - 86.12MB & 83.24 Minutes",
+  price: "GH₵5",
+  active: true,
+},
+
+{
+  id: 59,
+  category: "Mashup",
+  name:
+    "¢6 - 103.35MB & 99.88 Minutes",
+  price: "GH₵6",
+  active: true,
+},
+
+{
+  id: 60,
+  category: "Mashup",
+  name:
+    "¢7 - 120.27MB & 116.53 Minutes",
+  price: "GH₵7",
+  active: true,
+},
+
+{
+  id: 61,
+  category: "Mashup",
+  name:
+    "¢8 - 137MB & 133.18 Minutes",
+  price: "GH₵8",
+  active: true,
+},
+
+{
+  id: 62,
+  category: "Mashup",
+  name:
+    "¢9 - 155.02MB & 149.83 Minutes",
+  price: "GH₵9",
+  active: true,
+},
+
+{
+  id: 63,
+  category: "Mashup",
+  name:
+    "¢10 - 180.72MB & 173.39 Minutes",
+  price: "GH₵10",
+  active: true,
+},
+
+{
+  id: 64,
+  category: "Mashup",
+  name:
+    "¢15 - 271.07MB & 260.08 Minutes",
+  price: "GH₵15",
+  active: true,
+},
+
+{
+  id: 65,
+  category: "Mashup",
+  name:
+    "¢20 - 361.43MB & 346.78 Minutes",
+  price: "GH₵20",
+  active: true,
+},
+
+{
+  id: 66,
+  category: "Mashup",
+  name:
+    "¢25 - 451.79MB & 433.48 Minutes",
+  price: "GH₵25",
+  active: true,
+},
+
+{
+  id: 67,
+  category: "Mashup",
+  name:
+    "¢29.99 - 541.97MB & 520 Minutes",
+  price: "GH₵29.99",
+  active: true,
+},
+
+// =========================
+// TELECEL VOICE + SMS
+// =========================
+
+{
+  id: 68,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢1 - 21 Minutes + 5 SMS",
+  price: "GH₵1",
+  active: true,
+},
+
+{
+  id: 69,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢2 - 43 Minutes + 5 SMS",
+  price: "GH₵2",
+  active: true,
+},
+
+{
+  id: 70,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢3 - 64 Minutes + 5 SMS",
+  price: "GH₵3",
+  active: true,
+},
+
+{
+  id: 71,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢4 - 84 Minutes + 5 SMS",
+  price: "GH₵4",
+  active: true,
+},
+
+{
+  id: 72,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢5 - 114 Minutes + 10 SMS",
+  price: "GH₵5",
+  active: true,
+},
+
+{
+  id: 73,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢6 - 137 Minutes + 10 SMS",
+  price: "GH₵6",
+  active: true,
+},
+
+{
+  id: 74,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢7 - 160 Minutes + 10 SMS",
+  price: "GH₵7",
+  active: true,
+},
+
+{
+  id: 75,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢8 - 183 Minutes + 10 SMS",
+  price: "GH₵8",
+  active: true,
+},
+
+{
+  id: 76,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢9 - 206 Minutes + 10 SMS",
+  price: "GH₵9",
+  active: true,
+},
+
+{
+  id: 77,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢10 - 236 Minutes + 50 SMS",
+  price: "GH₵10",
+  active: true,
+},
+
+{
+  id: 78,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢15 - 354 Minutes + 50 SMS",
+  price: "GH₵15",
+  active: true,
+},
+
+{
+  id: 79,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢20 - 486 Minutes + 50 SMS",
+  price: "GH₵20",
+  active: true,
+},
+
+{
+  id: 80,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢25 - 607 Minutes + 50 SMS",
+  price: "GH₵25",
+  active: true,
+},
+
+{
+  id: 81,
+  category:
+    "Telecel Voice + SMS",
+  name:
+    "¢29.99 - 729 Minutes + 50 SMS",
+  price: "GH₵29.99",
+  active: true,
+},
+
+// =========================
+// TELECEL VOICE + DATA + SMS
+// =========================
+
+{
+  id: 82,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢1 - 16 Minutes + 17.83MB + 5 SMS",
+  price: "GH₵1",
+  active: true,
+},
+
+{
+  id: 83,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢2 - 32 Minutes + 35.66MB + 5 SMS",
+  price: "GH₵2",
+  active: true,
+},
+
+{
+  id: 84,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢3 - 48 Minutes + 53.49MB + 5 SMS",
+  price: "GH₵3",
+  active: true,
+},
+
+{
+  id: 85,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢4 - 64 Minutes + 71.32MB + 5 SMS",
+  price: "GH₵4",
+  active: true,
+},
+
+{
+  id: 86,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢5 - 85 Minutes + 94.72MB + 10 SMS",
+  price: "GH₵5",
+  active: true,
+},
+
+{
+  id: 87,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢6 - 102 Minutes + 113.66MB + 10 SMS",
+  price: "GH₵6",
+  active: true,
+},
+
+{
+  id: 88,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢7 - 119 Minutes + 132.6MB + 10 SMS",
+  price: "GH₵7",
+  active: true,
+},
+
+{
+  id: 89,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢8 - 136 Minutes + 151.54MB + 10 SMS",
+  price: "GH₵8",
+  active: true,
+},
+
+{
+  id: 90,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢9 - 153 Minutes + 170.49MB + 10 SMS",
+  price: "GH₵9",
+  active: true,
+},
+
+{
+  id: 91,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢10 - 175 Minutes + 195MB + 50 SMS",
+  price: "GH₵10",
+  active: true,
+},
+
+{
+  id: 92,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢15 - 262 Minutes + 292.5MB + 50 SMS",
+  price: "GH₵15",
+  active: true,
+},
+
+{
+  id: 93,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢20 - 359 Minutes + 401.15MB + 50 SMS",
+  price: "GH₵20",
+  active: true,
+},
+
+{
+  id: 94,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢25 - 449 Minutes + 501.43MB + 50 SMS",
+  price: "GH₵25",
+  active: true,
+},
+
+{
+  id: 95,
+  category:
+    "Telecel Voice + Data + SMS",
+  name:
+    "¢29.99 - 539 Minutes + 601.52MB + 50 SMS",
+  price: "GH₵29.99",
+  active: true,
+},
+    
     // =========================
     // AIRTIME
     // =========================
